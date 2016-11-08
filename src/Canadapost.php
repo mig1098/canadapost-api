5c29ddadaa513020f32ab8ae3453a040d1f52714
miglio esaud <mig1098@hotmail.com>
2016-11-08 15:22:07 -0500

    Create Canadapost.php

---
+++ src/Canadapost.php
@@ -0,0 +1,12 @@
+<?php
+namespace Canadapost;
+use Canadapost\Rates;
+class Canadapost{
+    public function __construct($data){
+        //
+    }
+    
+    public function getRates($data){
+        //
+    }
+}
